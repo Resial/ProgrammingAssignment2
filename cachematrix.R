55b3cdee2f240c5ee700f9cb1bb5d18ef593435a
Renata Almeida <renatadeal@gmail.com>
2016-09-15 21:53:13 +0200

    Renata's Matrix
    Assignment Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The function makeCacheMatrix follows the structure from the one on the assignment instructions.
+## 
 
-## Write a short comment describing this function
+## This function assigns value z to function x, thus creating a special object that cache a matrix inverse. Variable MInv means matrix inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+	MInv <- NULL
+    set <- function (z) { x <<- z
+        MInv <<-NULL }
+        
+    get <- function() x
+    setMInv <- function(inverse) Minv <<- inverse
+    getMInv <- function() Minv
+    
+    list (set = set, get = get,
+          setMInv = setMInv,
+          getMInv = getMInv)
 
 }
 
 
-## Write a short comment describing this function
+## This function compute the inverse of the special matrix object.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        MInv <- x$getMInv ()
+        if(!is.null(MInv)) {
+        	message("getting cached data")
+        	return(MInv)
+        }
+        data <- x$get()
+        MInv <- solve(data,...)
+        x$setMInv(MInv)
+        MInv
 }
